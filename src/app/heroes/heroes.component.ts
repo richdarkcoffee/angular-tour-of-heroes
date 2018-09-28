cfc94ca8b2acf677da18c3afa17d59a14f0d55f4
Richard Coffey <richardc@Richards-MacBook-Pro.local>
2018-09-27 18:54:32 -0700

    Got to Step 7 in tutorial

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,42 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-// It was important to not use .js here and just state the object file name
-import { Hero } from '../hero';
-// import { HEROES } from '../mock-heroes';
-// Changed the import of HEROES because we're going to use a service
-import { HeroService } from '../hero.service';
+  import { Component, OnInit } from '@angular/core';
+  // It was important to not use .js here and just state the object file name
+  import { Hero } from '../hero';
+  // import { HEROES } from '../mock-heroes';
+  // Changed the import of HEROES because we're going to use a service
+  import { HeroService } from '../hero.service';
 
-@Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
-})
-export class HeroesComponent implements OnInit {
+  @Component({
+    selector: 'app-heroes',
+    templateUrl: './heroes.component.html',
+    styleUrls: ['./heroes.component.css']
+  })
+  export class HeroesComponent implements OnInit {
+    heroes: Hero[];
   
-  // heroes = HEROES;
-  // Replaced definition of heroes property with:
-  heroes: Hero[];
-
-  // hero: Hero = {
-  //   id: 1,
-  //   name: 'Windstorm'
-  // };
-
-  selectedHero: Hero;
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
-  constructor(private heroService: HeroService) { }
+    constructor(private heroService: HeroService) { }
   
-  getHeroes(): void {
-    this.heroService.getHeroes()
-    .subscribe(heroes => this.heroes = heroes);
-  }
-  ngOnInit() {
-    this.getHeroes();
-  }
-
+    ngOnInit() {
+      this.getHeroes();
+    }
   
-
-}
+    getHeroes(): void {
+      this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+    }
+  }
